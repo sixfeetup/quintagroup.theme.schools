9149908c3dc3aefc0fb493a575ba978061c9496c
gvizdyk <gvizdyk@quintagroup.com>
2011-06-09 15:24:18 +0000

    updated theme

--- quintagroup/theme/schools/__init__.py
+++ quintagroup/theme/schools/__init__.py
@@ -1,20 +1,4 @@
-"""Main product initializer
-"""
-
-from zope.i18nmessageid import MessageFactory
-
-# Define a message factory for when this product is internationalised.
-# This will be imported with the special name "_" in most modules. Strings
-# like _(u"message") will then be extracted by i18n tools for translation.
-
-GLOBALS = globals()
-
-schoolsMessageFactory = MessageFactory('quintagroup.theme.schools')
+  # -*- extra stuff goes here -*-
 
 def initialize(context):
-    """Initializer called when used as a Zope 2 product.
-
-    This is referenced from configure.zcml. Regstrations as a "Zope 2 product"
-    is necessary for GenericSetup profiles to work, for example.
-    """
-
+    """Initializer called when used as a Zope 2 product."""
