2f4f68a0b5cc956cbf7c879354a39cebd2e8a777
naivok <naivok@quintagroup.com>
2013-03-21 14:31:51 +0000

    added 6.1.2 versions

---
+++ README.txt
@@ -0,0 +1,3 @@
+Schools Theme is a free diazo theme for Plone 4.1.
+
+For theme features - see README.txt file in quintagroup -> theme -> schools folder. 
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+version = '6.1'
+
+setup(name='quintagroup.theme.schools',
+      version=version,
+      description="Free Diazo theme for Plone",
+      long_description=open(os.path.join("quintagroup", "theme", "schools", "README.txt")).read() + "\n\n" +
+                       open(os.path.join("docs", "INSTALL.txt")).read() + "\n\n"+
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='web zope plone theme diazo quintagroup',
+      author='Quintagroup',
+      author_email='skins@quintagroup.com',
+      url='http://themes.quintagroup.com/product/schools',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quintagroup', 'quintagroup.theme',],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'plone.app.theming',
+                        # -*- Extra requirements: -*-
+                        ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
